64b7b7732cd591082181daca73890ce4b45da573
Lokesh Verma <lokeshv613@gmail.com>
2025-07-05 15:22:41 +0530

    add landing page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext";
+import 'aos/dist/aos.css';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
